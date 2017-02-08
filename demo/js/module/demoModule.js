6c27f390922105fdcdcd4c883df19d4a95068aeb
WeijiaoJhao <1713161@gmail.com>
2017-02-08 16:21:39 +0800

    搬移directive檔案

--- demo/js/module/demoModule.js
+++ demo/js/module/demoModule.js
@@ -7,100 +7,24 @@ define(['bluetech'], function(bluetech) {
 
 		$locationProvider.hashPrefix('');
 		$routeProvider
+				// .when('/', {
+				// 	templateUrl: "../demo/view/index.html"
+				// })
 				.when('/', {
-					templateUrl: "../template/index.html"
-				})
-				.when('/grids', {
-					templateUrl: '../template/grids.html'
-				})
-				.when('/form', {
-					templateUrl: '../template/form.html'
-				})
-				.when('/form_advanced', {
-					templateUrl: '../template/form_advanced.html'
-				})
-				.when('/form_layout', {
-					templateUrl: '../template/form_layout.html'
-				})
-				.when('/form_buttons', {
-					templateUrl: '../template/form_buttons.html'
+					templateUrl: "../demo/view/datepicker.html"
 				})
 				.when('/datepicker', {
-					templateUrl: '../template/datepicker.html'
+					templateUrl: '../demo/view/datepicker.html'
 				})
 				.when('/datepicker_range', {
-					templateUrl: '../template/datepicker_range.html'
-				})
-				.when('/glyphicons', {
-					templateUrl: '../template/glyphicons.html'
-				})
-				.when('/icons', {
-					templateUrl: '../template/icons.html'
-				})
-				.when('/general_elements', {
-					templateUrl: '../template/general_elements.html'
-				})
-				.when('/switch', {
-					templateUrl: '../template/switch.html'
-				})
-				.when('/list', {
-					templateUrl: '../template/list.html'
-				})
-				.when('/gridslider', {
-					templateUrl: '../template/gridslider.html'
-				})
-				.when('/panel', {
-					templateUrl: '../template/panel.html'
-				})
-				.when('/dragDrop', {
-					templateUrl: '../template/dragDrop.html'
-				})
-				.when('/blockUI', {
-					templateUrl: '../template/blockUI.html'
-				})
-				.when('/echarts', {
-					templateUrl: '../template/echarts.html'
-				})
-				.when('/echarts2', {
-					templateUrl: '../template/echarts2.html'
-				})
-				.when('/tables', {
-					templateUrl: '../template/tables.html'
-				})
-				.when('/tables_dynamic', {
-					templateUrl: '../template/tables_dynamic.html'
+					templateUrl: '../demo/view/datepicker_range.html'
 				})
 				.when('/pagination', {
-					templateUrl: '../template/pagination.html'
-				})
-				.when('/fixed_sidebar', {
-					templateUrl: '../template/fixed_sidebar.html'
-				})
-				.when('/fixed_footer', {
-					templateUrl: '../template/fixed_footer.html'
-				})
-				.when('/notifications', {
-					templateUrl: '../template/notifications.html'
-				})
-				.when('/alert', {
-					templateUrl: '../template/alert.html'
-				})
-				.when('/projects', {
-					templateUrl: '../template/projects.html'
-				})
-				.when('/level2', {
-					templateUrl: '../template/level2.html'
-				})
-				.when('/component', {
-					templateUrl: '../template/component.html'
-				})
-				.when('/tables_dynamic2', {
-					templateUrl: '../template/tables_dynamic2.html'
+					templateUrl: '../demo/view/pagination.html'
 				})
 				.otherwise({
 					redirectTo: '/'
 				});
-				
 		}]);
 
     return app;
