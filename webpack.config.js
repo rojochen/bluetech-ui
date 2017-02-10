ba6d84b7239808949230716e6a951a2f85ed6fd1
Louis <neighborhood0830@gmail.com>
2017-02-10 11:03:54 +0800

    edit webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,49 @@
 var webpack = require('webpack'),
     ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require('path');
 
 module.exports = {
-    devtool: 'eval',
+    debug: true,
+    devtool: '#eval-source-map',
+    context: path.join(__dirname, 'app', 'js'),
     entry: {
-        bluetechUI: './src/js/app.js',
-        bluetechStyle: './src/js/style.js'
+        bluetechUI: ['webpack/hot/dev-server',
+            'webpack-hot-middleware/client', __dirname + '/src/js/app.js'
+        ],
+        bluetechStyle: ['webpack/hot/dev-server',
+            'webpack-hot-middleware/client', __dirname + '/src/js/style.js'
+        ]
     },
     output: {
-        path: 'dist/js',
+        path: __dirname + '/dist/js',
+        publicPath: '/dist/',
         filename: '[name].min.js',
     },
-    resolve:{
+    resolve: {
         modulesDirectories: ['src/js/', 'node_modules'],
         alias: {
             bluetech: 'bluetech/dist/js/bluetech'
         }
     },
     module: {
-        loaders: [
-            {
-                test: /\.(png|gif)$/,
-                loader: 'url-loader?limit=100000'
-            }, {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract("style-loader", "css?minimize!sass")
-            }, {
-                test: /\.scss$/,
-                loader: ExtractTextPlugin.extract("style", "css?minimize!sass")
-            }, {
-                test: /\.(jpg|woff|svg|ttf|eot)([\?]?.*)$/,
-                loader: "file-loader?name=../css/img/[name].[ext]"
-            }
-        ]
+        loaders: [{
+            test: /\.(png|gif)$/,
+            loader: 'url-loader?limit=100000'
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract("style-loader", "css?minimize!sass")
+        }, {
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract("style", "css?minimize!sass")
+        }, {
+            test: /\.(jpg|woff|svg|ttf|eot)([\?]?.*)$/,
+            loader: "file-loader?name=../css/img/[name].[ext]"
+        }]
     },
     plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
         new webpack.optimize.UglifyJsPlugin({
             debug: true,
             minimize: true,
@@ -47,15 +56,8 @@ module.exports = {
             }
         }),
         new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.OccurrenceOrderPlugin(true),
         new ExtractTextPlugin("../css/bluetechStyle.min.css", {
             allChunks: true
         })
-    ],
-    devServer: {
-        hot: true,
-        inline: true,
-        port: 3000,
-        contentBase: ["node_modules", "demo", "demo/view/", 'dist']
-    }
+    ]
 };
