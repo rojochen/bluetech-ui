0ff2900ea29ba7cb909871646c99265444d907ec
Louis <neighborhood0830@gmail.com>
2017-02-20 15:20:41 +0800

    edit gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,23 @@ const gulp = require('gulp'),
 
 
 
-const btDirective = ["src/js/directive/directive.js", "src/js/directive/*.js"];
+// const btDirective = ["src/js/directive/directive.js", "src/js/directive/*.js"];
 
-gulp.task('build-directive', () => gulp.src(btDirective)
-    .pipe(concat('btDirective.js'))
-    .pipe(gulp.dest('src/js/directive/dist/')));
+// gulp.task('build-directive', () => gulp.src(btDirective)
+//     .pipe(concat('btDirective.js'))
+//     .pipe(gulp.dest('src/js/directive/dist/')));
 
 
-const btService = ["src/js/service/service.js", "src/js/service/*.js"];
+// const btService = ["src/js/service/service.js", "src/js/service/*.js"];
 
-gulp.task('build-Service', () => gulp.src(btService)
-    .pipe(concat('btService.js'))
-    .pipe(gulp.dest('src/js/service/dist/')));
+// gulp.task('build-Service', () => gulp.src(btService)
+//     .pipe(concat('btService.js'))
+//     .pipe(gulp.dest('src/js/service/dist/')));
 
-gulp.task('all', ['build-directive', 'build-Service']);
+// gulp.task('all', ['build-directive', 'build-Service']);
+
+const buildJs = ["src/js/prefix.js", "src/js/directive/*.js", "src/js/service/*.js", "src/js/suffix.js"];
+
+gulp.task('build-js', () => gulp.src(buildJs)
+    .pipe(concat('buletech-ui.js'))
+    .pipe(gulp.dest('src/js/')));
