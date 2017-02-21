ed446323bb093db592cb229235e125f422f2bcbf
rogerchen <jjc.roger@gmail.com>
2017-02-21 10:58:15 +0800

    change class

--- src/js/service/btpaginationService.js
+++ src/js/service/btpaginationService.js
@@ -1,20 +1,38 @@
 (()=>{
-    function btpaginationService1($log) {
-        let paginationInfo = null;
-        return {
-            setInfo(data) {
-                paginationInfo = angular.copy(data);
-            },
-            getInfo() {
-                return paginationInfo;
-            },
-            goFirst(id) {
-                $(`#${id}`).triggerHandler('goFirst');
-            }
-        };
+    // function btpaginationService1($log) {
+    //     let paginationInfo = null;
+    //     return {
+    //         setInfo(data) {
+    //             paginationInfo = angular.copy(data);
+    //         },
+    //         getInfo() {
+    //             return paginationInfo;
+    //         },
+    //         goFirst(id) {
+    //             $(`#${id}`).triggerHandler('goFirst');
+    //         }
+    //     };
         
+    // }
+    class PanginationService{
+         constructor($log) {
+            this.$log = $log;
+            this.paginationInfo = null;
+         }
+         setInfo(paginationInfo){
+             this.paginationInfo = angular.copy(paginationInfo);
+         }
+         getInfo(){
+             return this.paginationInfo;
+         }
+         goFirst(id){
+            
+              $(`#${id}`).triggerHandler('goFirst');
+         }
+          
     }
-    btpaginationService1.$inject = ['$log'];
-    angular.module("bluetechUI").factory('paginationService', btpaginationService1);
+
+    PanginationService.$inject = ['$log'];
+    angular.module("bluetechUI").factory('paginationService',($log)=> new PanginationService($log));
 })();
 // }
--- src/js/service/modalService.js
+++ src/js/service/modalService.js
@@ -1,14 +1,25 @@
 (() => {
-    function btmodalService($log) {
-        return {
-            openModal(id) {
-                $(`#${id}`).modal('show');
-            },
-            closeModal(id) {
+    // function btmodalService($log) {
+    //     return {
+    //         openModal(id) {
+    //             $(`#${id}`).modal('show');
+    //         },
+    //         closeModal(id) {
+    //             $(`#${id}`).modal('hide');
+    //         }
+    //     };
+    // }
+    class ModalService{
+        constructor($log){
+            this.$log = $log;
+        }
+        openModal(id){
+            $(`#${id}`).modal('show');
+        }
+        closeModal(id) {
                 $(`#${id}`).modal('hide');
             }
-        };
     }
     btmodalService.$inject = ['$log'];
-    angular.module("bluetechUI").factory('modalService', btmodalService);
+    angular.module("bluetechUI").factory('modalService', ($log)=> new ModalService($log));
 })();
