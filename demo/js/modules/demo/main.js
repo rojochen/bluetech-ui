0683fec1f39546a742e6338a8907450e2682b740
rogerchen <jjc.roger@gmail.com>
2017-02-21 19:25:35 +0800

    change bluetechUI path

--- demo/js/modules/demo/main.js
+++ demo/js/modules/demo/main.js
@@ -1,5 +1,6 @@
 import ngRoute from 'angular-route/angular-route.min';
 import SliderMenu from './controllers/SliderMenu.js';
+import bluetechUI from './../../../../dist/js/bluetechUI.min.js';
 
 export default  appModule = angular.module('bluetechUI.demo',['bluetechUI','ngRoute'])
      .controller('sliderMenu',($timeout)=> new SliderMenu($timeout));
