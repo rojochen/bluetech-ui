ee3629fd21ea614826731f802bdb41cf7b9ee5e8
rogerchen <jjc.roger@gmail.com>
2017-02-22 12:25:49 +0800

    add route

--- demo/js/modules/demo/main.js
+++ demo/js/modules/demo/main.js
@@ -1,8 +1,10 @@
 import {bluetechUI} from './../../../../dist/js/bluetechUI.min.js';
 import {ngRoute} from 'angular-route/angular-route.min';
+import {RouteConfig} from './config/RouteConfig';
 import {SliderMenu} from './controllers/SliderMenu.js';
 
 
 export const  appModule = angular.module('bluetechUI.demo',['bluetechUI','ngRoute'])
+     .config(($routeProvider,$locationProvider)=>new RouteConfig($routeProvider,$locationProvider))
      .controller('sliderMenu',($timeout)=> new SliderMenu($timeout))
      .name;
