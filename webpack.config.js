71664319365a6e9999976839dcc42a931713a74a
Louis <neighborhood0830@gmail.com>
2017-03-01 11:25:44 +0800

    edit webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,14 @@ module.exports = {
     devtool: 'eval',
     entry: {
         bluetechUI: './src/js/app.js',
+        "bluetechUI.min": './src/js/app.js',
         bluetechStyle: './src/js/style.js'
     },
     output: {
         path: 'dist/js',
-        filename: '[name].min.js',
+        filename: '[name].js',
     },
-    resolve:{
+    resolve: {
         modules: ['src/js/', 'src/css/', 'node_modules'],
         extensions: ['.js', '.css'],
         alias: {
@@ -19,70 +20,62 @@ module.exports = {
         }
     },
     resolveLoader: {
-        moduleExtensions: ['-loader']  //讓loader不用打
+        moduleExtensions: ['-loader'] //讓loader不用打
     },
     module: {
         rules: [{
-                test: /\.(png|gif)$/,
-                use: 'url?limit=100000'
-            }, {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style',
-                    use: [{
-                        loader: 'css',
-                        query: {
-                            import: true,
-                            modules: false,
-                            sourceMaps: true
-                        }
-                    }, "sass"]
-                })
-            }, {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style',
-                    use: [{
-                        loader: 'css',
-                        query: {
-                            import: true,
-                            modules: false,
-                            sourceMaps: true
-                        }
-                    }, "sass"]
-                })
-            }, {
-                test: /\.(jpg|woff|svg|ttf|png|eot)([\?]?.*)$/,
-                use: "file?name=../css/img/[name].[ext]"
-            }, {
-                test: /\.js$/,
-                exclude: /(node_modules|vendors)/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015']
-                }
-            }]
+            test: /\.(png|gif)$/,
+            use: 'url?limit=100000'
+        }, {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style',
+                use: [{
+                    loader: 'css',
+                    query: {
+                        import: true,
+                        modules: false,
+                        sourceMaps: true
+                    }
+                }, "sass"]
+            })
+        }, {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style',
+                use: [{
+                    loader: 'css',
+                    query: {
+                        import: true,
+                        modules: false,
+                        sourceMaps: true
+                    }
+                }, "sass"]
+            })
+        }, {
+            test: /\.(jpg|woff|svg|ttf|png|eot)([\?]?.*)$/,
+            use: "file?name=../css/img/[name].[ext]"
+        }, {
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            loader: 'babel',
+            query: {
+                presets: ['es2015']
+            }
+        }]
     },
     plugins: [
-        // new webpack.HotModuleReplacementPlugin(),  //?
-        // new webpack.LoaderOptionsPlugin({  //有用？
-        //     minimize: true
-        // }),
         new ExtractTextPlugin({
             filename: "../css/bluetechStyle.min.css",
             // disable: false,
             allChunks: true
         }),
-        new webpack.optimize.UglifyJsPlugin({  //壓縮
-            beautify: false,
-            sourceMap: false,
-            comments: false,  //删除所有的注释
-            compress: {
-                warnings: false,  //在UglifyJs删除没有用到的代码时不输出警告
-                drop_console: true,  //删除console语句
-                collapse_vars: true,  //内嵌定义了但是只用到一次的变量
-                reduce_vars: true,  //提取出出现多次但是没有定义成变量去引用的静态值
-            }
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            include: /\.min\.js$/,
+            minimize: true
         })
     ],
     devServer: {
--- webpack.demo.config.js
+++ webpack.demo.config.js
@@ -10,7 +10,7 @@ module.exports = {
     output: {
         path: 'demo/',
         filename: '[name].min.js',
-        publicPath:'/bluetech-ui/'
+        publicPath: '/bluetech-ui/'
     },
     resolve: {
         modules: ['src/js/', 'src/css/', 'node_modules'],
@@ -69,10 +69,9 @@ module.exports = {
             test: /\.(jpg|woff|svg|ttf|png|eot)([\?]?.*)$/,
             exclude: /(node_modules)/,
             use: "file?name=assets/[name].[ext]"
-        }
-        , {
+        }, {
             test: /\.js$/,
-            exclude: /(node_modules|vendors)/,
+            exclude: "node_modules",
             loader: 'babel',
             query: {
                 presets: ['es2015']
@@ -80,30 +79,18 @@ module.exports = {
         }]
     },
     plugins: [
-        // new webpack.HotModuleReplacementPlugin(),  //?
-        // new webpack.LoaderOptionsPlugin({  //有用？
-        //     minimize: true
-        // }),
         new ExtractTextPlugin({
             filename: "/css/bluetechStyle.min.css",
             // disable: false,
             allChunks: true
         }),
-         new webpack.optimize.UglifyJsPlugin({
-            include: /\.min\.js$/,
+        new webpack.optimize.UglifyJsPlugin({
+            include: /\.js$/,
             minimize: true
-        })
-        // new webpack.optimize.UglifyJsPlugin({  //壓縮
-        //     beautify: false,
-        //     sourceMap: false,
-        //     comments: false,  //删除所有的注释
-        //     compress: {
-        //         warnings: false,  //在UglifyJs删除没有用到的代码时不输出警告
-        //         drop_console: true,  //删除console语句
-        //         collapse_vars: true,  //内嵌定义了但是只用到一次的变量
-        //         reduce_vars: true,  //提取出出现多次但是没有定义成变量去引用的静态值
-        //     }
-        // })
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        }),
     ],
     devServer: {
         hot: true,
