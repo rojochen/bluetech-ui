16b2cc01b611c5cb545a7255304b3b2ebf64dd9b
yvonne6344 <yvonne791016@gmail.com>
2017-03-02 16:26:55 +0800

    練習活動群組設定

--- demo/js/modules/common/module.js
+++ demo/js/modules/common/module.js
@@ -1,16 +1,44 @@
-import {bluetechUI} from './../../../../dist/js/bluetechUI.min.js';
+import {
+    bluetechUI
+} from './../../../../dist/js/bluetechUI.min.js';
 // import angular from 'angular/angular.min.js';
-import {uiRouter} from 'angular-ui-router/release/angular-ui-router.min.js';
-import {RouteConfig} from './config/RouteConfig';
-import {SliderMenu} from './components/SliderMenu.js';
-import {DemoCtrl} from './controllers/DemoCtrl.js';
-import {BtFooter} from './components/Footer.js';
-import {Header} from './components/Header.js';
-import {ConfirmModal} from './components/ConfirmModal.js';
-import {Datepicker} from './components/Datepicker.js';
-import {DatepickerRange} from './components/DatepickerRange.js';
-import {Pagination} from './components/Pagination.js';
-import {PnotifyService} from './components/PnotifyService.js';
+import {
+    uiRouter
+} from 'angular-ui-router/release/angular-ui-router.min.js';
+import {
+    RouteConfig
+} from './config/RouteConfig';
+import {
+    SliderMenu
+} from './components/SliderMenu.js';
+import {
+    DemoCtrl
+} from './controllers/DemoCtrl.js';
+import {
+    BtFooter
+} from './components/Footer.js';
+import {
+    Header
+} from './components/Header.js';
+import {
+    ConfirmModal
+} from './components/ConfirmModal.js';
+import {
+    Datepicker
+} from './components/Datepicker.js';
+import {
+    DatepickerRange
+} from './components/DatepickerRange.js';
+import {
+    Pagination
+} from './components/Pagination.js';
+import {
+    PnotifyService
+} from './components/PnotifyService.js';
+import {
+    ActivityGroup
+} from './../yvonne/components/ActivityGroup.js';
+
 
 
 export const appModule = angular.module('bluetechUI.common', ['bluetechUI', 'ui.router'])
@@ -23,5 +51,6 @@ export const appModule = angular.module('bluetechUI.common', ['bluetechUI', 'ui.
     .component('datepickerRange', DatepickerRange)
     .component('pagination', Pagination)
     .component('pnotifyService', PnotifyService)
+    .component('activityGroup', ActivityGroup)
     .controller('demoCtrl', () => new DemoCtrl())
     .name;
