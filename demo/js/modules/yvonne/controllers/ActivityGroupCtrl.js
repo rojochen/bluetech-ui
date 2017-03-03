e87945bdb21a3d60928fb3ed12dbb503592f0a53
yvonne6344 <yvonne791016@gmail.com>
2017-03-03 18:35:24 +0800

    修改 ActivityGroup

---
+++ demo/js/modules/yvonne/controllers/ActivityGroupCtrl.js
@@ -0,0 +1,48 @@
+// ps. import 都在上方 export 都在下方
+import {
+    PortletCtrl
+} from "./../../common/controllers/PortletCtrl.js";
+
+class ActivityGroupCtrl extends PortletCtrl {
+    constructor() {
+        //  繼承了 PortletCtrl 可以直接使用他的function 請看html
+        // supre執行繼承function
+        super();
+        this.ActivityGroupCtrlFun();
+        console.log('123');
+    }
+
+    ActivityGroupCtrlFun() {
+        /*begin Portlet */
+        // $('.collapse-link').on('click', () => {
+        //     let $BOX_PANEL = $(this).closest('.x_panel'),
+        //         $ICON = $(this).find('i'),
+        //         $BOX_CONTENT = $BOX_PANEL.find('.x_content');
+
+        //     // fix for some div with hardcoded fix class
+        //     if ($BOX_PANEL.attr('style')) {
+        //         $BOX_CONTENT.slideToggle(200, () => {
+        //             $BOX_PANEL.removeAttr('style');
+        //         });
+        //     } else {
+        //         $BOX_CONTENT.slideToggle(200);
+        //         $BOX_PANEL.css('height', 'auto');
+        //     }
+
+        //     $ICON.toggleClass('fa-chevron-up fa-chevron-down');
+        // });
+
+        // $('.close-link').click(() => {
+        //     let $BOX_PANEL = $(this).closest('.x_panel');
+
+        //     $BOX_PANEL.remove();
+        // });
+        /* end Portlet*/
+    }
+}
+
+ActivityGroupCtrl.$inject = [];
+
+export {
+    ActivityGroupCtrl
+}
