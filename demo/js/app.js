77d7e3da99d885b600d60977f97e6f47e0957ef2
Louis <Louis@Louisxie-2.local>
2017-03-03 12:40:33 +0800

    merge develop

--- demo/js/app.js
+++ demo/js/app.js
@@ -1,8 +1,6 @@
 import bluetechUI from './../../dist/js/bluetechUI.min.js';
 import bluetechUICommon from './modules/common/module.js';
-
 import bluetechUIYvonne from './modules/yvonne/module.js'
-
 import bluetechStyle from './../../dist/css/bluetechStyle.min.css';
 
 
